b7743b9941971f1de83fad1dd3e04208df48afec
Abhinav <abhi2chat@gmail.com>
2016-04-13 19:43:10 -0400

    changed 5

--- topo.py
+++ topo.py
@@ -83,12 +83,9 @@ class QuaggaTopo(Topo):
             # Attach the quaggaContainer to the IXP Fabric Switch
             #self.addLink(quaggaContainer, ixpfabric)
 
-	self.addlink(quaggaNodes[0], quaggaNodes[1], intfname1='h1-eth0', params1={'ip':'172.0.1.1/24'}, intfname2='r1-eth1', params2={'ip':'172.0.1.2/24'})
-
-"""	self.addLink(H1,R1, intfname1='h1-eth0', params1={'ip':'172.0.1.1/24'}, intfname2='r1-eth1', params2={'ip':'172.0.1.2/24'})
-	self.addLink(R1,R2, intfname1='r1-eth1', params1={'ip':'172.0.1.2/24'}, intfname2='r2-eth0', params2={'ip':'172.0.2.2/24'})
-	self.addLink(R1,R3, intfname1='r1-eth2', params1={'ip':'172.0.1.2/24'}, intfname2='r3-eth0', params2={'ip':'172.0.3.2/24'})
-	self.addLink(R2,R4, intfname1='r2-eth1', params1={'ip':'172.0.2.2/24'}, intfname2='r4-eth0', params2={'ip':'172.0.4.2/24'})
-	self.addLink(R3,R4, intfname1='r3-eth0', params1={'ip':'172.0.3.2/24'}, intfname2='r4-eth1', params2={'ip':'172.0.4.2/24'})
-	self.addLink(H2,R4, intfname1='h2-eth0', params1={'ip':'172.0.4.1/24'}, intfname2='r4-eth2', params2={'ip':'172.0.4.2/24'})
-"""
+	self.addlink(quaggaNodes[0], quaggaNodes[1], intfName1='h1-eth0', params1={'ip':'172.0.1.1/24'}, intfName2='r1-eth1', params2={'ip':'172.0.1.2/24'})
+	self.addLink(quaggaNodes[1],quaggaNodes[4], intfName1='r1-eth1', params1={'ip':'172.0.1.2/24'}, intfName2='r2-eth0', params2={'ip':'172.0.2.2/24'})
+	self.addLink(quaggaNodes[1],quaggaNodes[5], intfName1='r1-eth2', params1={'ip':'172.0.1.2/24'}, intfName2='r3-eth0', params2={'ip':'172.0.3.2/24'})
+	self.addLink(quaggaNodes[4],quaggaNodes[3], intfName1='r2-eth1', params1={'ip':'172.0.2.2/24'}, intfName2='r4-eth0', params2={'ip':'172.0.4.2/24'})
+	self.addLink(quaggaNodes[5],quaggaNodes[3], intfName1='r3-eth0', params1={'ip':'172.0.3.2/24'}, intfName2='r4-eth1', params2={'ip':'172.0.4.2/24'})
+	self.addLink(quaggaNodes[2],quaggaNodes[3], intfName1='h2-eth0', params1={'ip':'172.0.4.1/24'}, intfName2='r4-eth2', params2={'ip':'172.0.4.2/24'})
